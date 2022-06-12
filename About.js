8f1119d91fab87ad44c043d21b7a82fb75bfd073
Fatehpreet512 <69321829+Fatehpreet512@users.noreply.github.com>
2022-06-12 11:05:54 +0530

    Create About.js

---
+++ About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className="container text-center">
+            <h3>About Us</h3>
+            <h5>We provide a Todo List</h5>
+        </div>
+    )
+}
